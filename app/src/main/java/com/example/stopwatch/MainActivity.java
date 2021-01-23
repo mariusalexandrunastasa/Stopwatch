17fe24ad70f161e072d0ce50e5ee4aadf1dba320
mariusalexandrunastasa <mariusalexandrunastasa@gmail.com>
2021-01-23 21:13:42 +0200

    added timer functionality

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.stopwatch;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.widget.Button;
+import android.widget.Chronometer;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 public class MainActivity extends AppCompatActivity {
 
     Button buttonStart, buttonStop;
+    Chronometer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,21 +21,26 @@ public class MainActivity extends AppCompatActivity {
 
         buttonStart = findViewById(R.id.StartButton);
         buttonStop = findViewById(R.id.StopButton);
+        timer = findViewById(R.id.elapsedTime);
+        AtomicLong base = new AtomicLong(0);
 
         buttonStop.setAlpha(0);
 
         buttonStart.setOnClickListener(v -> {
             buttonStop.animate().alpha(1).translationY(-173).setDuration(300).start();
             buttonStart.animate().alpha(0).setDuration(300).start();
+            timer.setBase(SystemClock.elapsedRealtime() + base.get());
+            timer.start();
+            base.set(0);
         });
 
         buttonStop.setOnClickListener(v -> {
             buttonStop.animate().alpha(0).translationY(173).setDuration(300).start();
             buttonStart.animate().alpha(1).setDuration(300).start();
+            base.set(timer.getBase() - SystemClock.elapsedRealtime());
+            timer.stop();
         });
 
     }
-
-
 
 }
